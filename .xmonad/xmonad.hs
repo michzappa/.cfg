36e1011bab3d7f9993203e2d48137cd6637cc719
Michael Zappa <zapprich@gmail.com>
2020-07-26 19:40:17 +0000

    search engine in xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,15 +8,21 @@ import XMonad.Util.SpawnOnce
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 import Graphics.X11.ExtraTypes.XF86
-import XMonad.Util.CustomKeys
 import XMonad.Hooks.EwmhDesktops
 import Control.Monad (when, join)
 import Data.Maybe (maybeToList)
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Actions.Submap
+import qualified XMonad.Actions.Search as S
+import XMonad.Prompt
+import XMonad.Prompt.FuzzyMatch
+import Control.Arrow (first)
 
 myTerminal :: String
 myTerminal = "kitty"
+
+myFont :: String
+myFont = "xft:System San Fransisco Display:pixelsize=16:antialias=true:hinting=true"
 
 myStartupHook :: X ()
 myStartupHook = do
@@ -42,6 +48,90 @@ addNETSupported x   = withDisplay $ \dpy -> do
        sup <- (join . maybeToList) <$> getWindowProperty32 dpy a_NET_SUPPORTED r
        when (fromIntegral x `notElem` sup) $
          changeProperty32 dpy r a_NET_SUPPORTED a propModeAppend [fromIntegral x]
+
+myPromptConfig :: XPConfig
+myPromptConfig = def
+      { font                = myFont
+      , bgColor             = "#2E3440"
+      , fgColor             = "#d0d0d0"
+      , bgHLight            = "#7895b3"
+      , fgHLight            = "#000000"
+      , borderColor         = "#2E3440"
+      , promptBorderWidth   = 0
+      , promptKeymap        = myPromptKeymap
+      , position            = CenteredAt { xpCenterY = 0.42, xpWidth = 0.3 }
+      , height              = 20
+      , historySize         = 256
+      , historyFilter       = id
+      , defaultText         = []
+      , autoComplete        = Just 100000  -- set Just 100000 for .1 sec
+      , showCompletionOnTab = False
+      , searchPredicate     = fuzzyMatch
+      , alwaysHighlight     = True
+      , maxComplRows        = Nothing      -- set to Just 5 for 5 rows
+      }
+
+-- autocomplete turned off for Searching, not launching
+myPromptConfig' :: XPConfig
+myPromptConfig' = myPromptConfig
+      { autoComplete        = Nothing
+      }
+ 
+myPromptKeymap :: M.Map (KeyMask,KeySym) (XP ())
+myPromptKeymap = M.fromList $
+     map (first $ (,) controlMask)   -- control + <key>
+     [ (xK_z, killBefore)            -- kill line backwards
+     , (xK_k, killAfter)             -- kill line forwards
+     , (xK_a, startOfLine)           -- move to the beginning of the line
+     , (xK_e, endOfLine)             -- move to the end of the line
+     , (xK_m, deleteString Next)     -- delete a character foward
+     , (xK_b, moveCursor Prev)       -- move cursor forward
+     , (xK_f, moveCursor Next)       -- move cursor backward
+     , (xK_BackSpace, killWord Prev) -- kill the previous word
+     , (xK_y, pasteString)           -- paste a string
+     , (xK_g, quit)                  -- quit out of prompt
+     , (xK_bracketleft, quit)
+     ]
+     ++
+     map (first $ (,) mod1Mask)       -- meta key + <key>
+     [ (xK_BackSpace, killWord Prev) -- kill the prev word
+     , (xK_f, moveWord Next)         -- move a word forward
+     , (xK_b, moveWord Prev)         -- move a word backward
+     , (xK_d, killWord Next)         -- kill the next word
+     , (xK_n, moveHistory W.focusUp')   -- move up thru history
+     , (xK_p, moveHistory W.focusDown') -- move down thru history
+     ]
+     ++
+     map (first $ (,) 0) -- <key>
+     [ (xK_Return, setSuccess True >> setDone True)
+     , (xK_KP_Enter, setSuccess True >> setDone True)
+     , (xK_BackSpace, deleteString Prev)
+     , (xK_Delete, deleteString Next)
+     , (xK_Left, moveCursor Prev)
+     , (xK_Right, moveCursor Next)
+     , (xK_Home, startOfLine)
+     , (xK_End, endOfLine)
+     , (xK_Down, moveHistory W.focusUp')
+     , (xK_Up, moveHistory W.focusDown')
+     , (xK_Escape, quit)
+     ]
+
+archwiki, reddit :: S.SearchEngine
+
+archwiki = S.searchEngine "archwiki" "https://wiki.archlinux.org/index.php?search="
+reddit   = S.searchEngine "reddit" "https://www.reddit.com/search/?q="
+
+searchList :: [(String, S.SearchEngine)]
+searchList = [ ("a", archwiki)
+             , ("d", S.duckduckgo)
+             , ("g", S.google)
+             , ("h", S.hoogle)
+             , ("i", S.images)
+             , ("r", reddit)
+             , ("w", S.wikipedia)
+             , ("y", S.youtube)
+             , ("z", S.amazon)
+             ]
 
 myManageHook :: Query (Endo WindowSet)
 myManageHook = composeAll
@@ -101,6 +191,8 @@ myKeys =
       ("M-S-" ++ key, (windows $ W.shift ws))
       | (key, ws) <- myExtraWorkspaces
     ]
+    ++ [("M-s " ++ key, S.promptSearch myPromptConfig' engine) | (key, engine) <- searchList ]
+    ++ [("M-S-s " ++ key, S.selectSearch engine) | (key, engine) <- searchList ]
 
 myExtraWorkspaces :: [(String, WorkspaceId)]
 myExtraWorkspaces = [("0", "0")]
