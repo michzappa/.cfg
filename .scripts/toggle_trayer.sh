0f1ac74128b533ffd85c51911ad898937a069163
Michael Zappa <zapprich@gmail.com>
2020-08-09 22:07:40 -0500

    trayer move

--- .scripts/toggle_trayer.sh
+++ .scripts/toggle_trayer.sh
@@ -8,9 +8,9 @@ else
         --align right \
         --edge top \
         --width 5 \
-        --height 19 \
+        --height 22 \
         --distancefrom right \
-        --distance 450 \
+        --distance 620 \
         --tint 0x2E3440 \
         --transparent true \
         --alpha 0
