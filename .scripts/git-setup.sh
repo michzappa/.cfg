db7c68493f6c700b493bd63a5b5be8d03397571f
Michael Zappa <zapprich@gmail.com>
2020-08-29 15:39:06 -0500

    git setup script

---
+++ .scripts/git-setup.sh
@@ -0,0 +1,4 @@
+git config --global user.name "Michael Zappa"
+git config --global user.email "zapprich@gmail.com"
+git config --global credential.helper store
+git config --global pull.rebase false
